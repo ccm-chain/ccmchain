31c59d95189d7a99cdfa706d67b10f155edc1b97
zeoio <kinsleer@outlook.com>
2020-08-22 10:52:35 +0800

    console: fix console print  welcome medadata

--- console/console.go
+++ console/console.go
@@ -280,9 +280,9 @@ func (c *Console) Welcome() {
 	if res, err := c.jsre.Run(`
 		var message = "instance: " + web3.version.node + "\n";
 		try {
-			message += "coinbase: " + eth.coinbase + "\n";
+			message += "coinbase: " + ccm.coinbase + "\n";
 		} catch (err) {}
-		message += "at block: " + eth.blockNumber + " (" + new Date(1000 * eth.getBlock(eth.blockNumber).timestamp) + ")\n";
+		message += "at block: " + ccm.blockNumber + " (" + new Date(1000 * ccm.getBlock(ccm.blockNumber).timestamp) + ")\n";
 		try {
 			message += " datadir: " + admin.datadir + "\n";
 		} catch (err) {}
